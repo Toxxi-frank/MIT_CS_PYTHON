06a55e5ab844c7b10a3dbe9c130b1bef2a022f10
Toxxi-frank <82706682+Toxxi-frank@users.noreply.github.com>
2021-10-02 21:13:33 +0700

    Update Problem1.py

--- PS4/Problem1.py
+++ PS4/Problem1.py
@@ -1,5 +1,21 @@
 # Problem #1: Scoring a word
-#
+
+# The first step is to implement some code that allows us to calculate the score for a single word.
+# The function getWordScore should accept as input a string of lowercase letters (a word)
+# and return the integer score for that word, using the game's scoring rules.
+
+# HINTS
+# You may assume that the input word is always either a string of lowercase letters, or the empty string "".
+# You will want to use the SCRABBLE_LETTER_VALUES dictionary defined at the top of ps4a.py.  You should not change its value.
+# Do not assume that there are always 7 letters in a hand!
+# The parameter n is the number of letters required for a bonus score (the maximum number of letters in the hand).
+# If you want to try playing your word game with n=10 or n=4, you will be able to do it by simply changing the value of HAND_SIZE!
+# Testing: If this function is implemented properly, and you run test_ps4a.py, you should see that the test_getWordScore() tests pass.
+# Also test your implementation of getWordScore, using some reasonable English words.
+# Fill in the code for getWordScore in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py
+# before pasting your function definition here.
+
+
 def getWordScore(word, n):
     """
     Returns the score for a word. Assumes the word is a valid word.
